59da694098f7fe37708db5f8db526770c2cec9c9
Andy Wickert <andy@northernwidget.com>
2017-01-02 22:42:08 -0600

    Clearer pathway to bootloader

--- NorthernWidgetAVR/bootloaders/optiboot/make_8MHz.sh
+++ NorthernWidgetAVR/bootloaders/optiboot/make_8MHz.sh
@@ -1,2 +1,4 @@
 # https://forum.arduino.cc/index.php?topic=379103.15
-make atmega328 AVR_FREQ=8000000L LED_START_FLASHES=3 BAUD_RATE=57600
+#make atmega328 AVR_FREQ=8000000L LED_START_FLASHES=3 BAUD_RATE=57600
+make atmega328_8MHz_external_oscillator BAUD_RATE=57600
+
